3875ee990d9ed52e37bfb280244f6bcdec6937ed
Jul <capsretri@gmail.com>
2024-09-26 22:09:18 +0800

    dekstop - web host

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'retri/code-dojo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '82c6bf216510c16fc956fa5fb7d22cbce91fcea7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'retri/code-dojo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '82c6bf216510c16fc956fa5fb7d22cbce91fcea7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
